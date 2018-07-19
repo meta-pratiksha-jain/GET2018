31fcf74f386cdb16e70503a482ac5dc2aa50b01d
meta-pratiksha-jain <41181700+meta-pratiksha-jain@users.noreply.github.com>
2018-07-18 22:12:58 -0700

    Add files via upload

---
+++ FixedProductPromotion.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class FixedProductPromotion implements Promotion
+{
+	double minimumPrice, discount;
+	@Override
+	public double getMinimumPrice()
+	{
+		return minimumPrice;
+	}
+	@Override
+	public void setMinimumPrice(double minimumPrice)
+	{
+		this.minimumPrice = minimumPrice;	
+	}
+	@Override
+	public double getFixedDiscount() 
+	{
+		return discount;
+	}
+	@Override
+	public void setFixedDiscount(double discount) 
+	{
+		this.discount = discount;
+		
+	}
+         /*
+	 * Checks whether promotion is applicable on promocode enterd by user
+	 * @param code is the promotion code that should be checked
+	 * @return returns true if promotion is applicable otherwise false
+	 */
+	@Override
+	public boolean isPromotionApplicable(String code)
+	{
+		Date currentDate=new Date();
+		boolean isApply=false;
+		for(PromotionEnum promo:PromotionEnum.values())
+		{
+			if(promo.getCode().equals(code))
+			{
+				if(currentDate.after(promo.getStartDate().getTime()) && currentDate.before(promo.getEndDate().getTime()))
+				{
+					isApply=true;
+				}
+				else
+				{
+					isApply=false;
+				}
+			}
+		}
+		return isApply;
+	}
+}
---
+++ Promotion.java
@@ -0,0 +1,12 @@
+public interface Promotion
+{
+    public double getMinimumPrice();
+	
+    public void setMinimumPrice(double minimumPrice);
+	
+    public double getFixedDiscount();
+	
+    public void setFixedDiscount(double discount);
+	
+    public boolean isPromotionApplicable(String code); 
+}
