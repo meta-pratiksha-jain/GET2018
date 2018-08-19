e4429a912ca42fbd410c6db83e8fbb89d841e9b8
meta-pratiksha-jain <41181700+meta-pratiksha-jain@users.noreply.github.com>
2018-08-20 01:20:39 +0530

    DBMS-Assignment-2

--- DBMS-Assignment-2/Assignment-1 Create tables.sql
+++ DBMS-Assignment-2/Assignment-1 Create tables.sql
@@ -11,6 +11,8 @@ quantity int not null,
 added_on_date Date not null,
 primary key(id)
 ); 
+ALTER TABLE Product
+ADD COLUMN is_active bool default true;
 
 CREATE TABLE Product_Image(
 product_id int not null,
@@ -19,7 +21,6 @@ image_tag varchar(20) not null,
 primary key(product_id,image_id),
 foreign key(product_id) references Product(id)
 );
-
 ALTER TABLE Product_Image
 ADD COLUMN image BLOB not null;
 
@@ -47,33 +48,13 @@ mobile_number int not null,
 email_id varchar(50) not null,
 primary key(id)
 );
-
-CREATE TABLE Orders(
-order_id int not null,
-shopper_id int not null,
-order_placing_date Date not null,
-bill float not null,
-primary key(order_id),
-foreign key(shopper_id) references Shopper(id)
-);
-
-CREATE TABLE Order_Product(
-order_id int not null,
-product_id int not null,
-ordered_quantity int not null,
-order_status varchar(20) not null,
-primary key(order_id,product_id),
-foreign key(order_id) references Orders(order_id),
-foreign key(product_id) references Product(id)
-);
-
-ALTER TABLE Product
-ADD COLUMN is_active bool default true;
-
 ALTER TABLE Shopper RENAME User;
-
 ALTER TABLE User
-ADD COLUMN is_admin bool not null;
+ADD COLUMN is_admin bool not null default false;
+ALTER TABLE User
+DROP Column mobile_number;
+ALTER TABLE User
+ADD COLUMN mobile_number BIGINT not null;
 
 CREATE TABLE Address(
 user_id int not null,
@@ -84,9 +65,29 @@ city varchar(100),
 state varchar(100),
 country varchar(100) default 'INDIA',
 zipcode int,
-primary key(user_id,address_id),
+primary key(address_id),
 foreign key(user_id) references User(id)
 );
 
-show tables;
+CREATE TABLE Orders(
+order_id int not null,
+shopper_id int not null,
+order_placing_date Date not null,
+bill float not null,
+primary key(order_id),
+foreign key(shopper_id) references User(id)
+);
 
+CREATE TABLE Order_Product(
+order_id int not null,
+product_id int not null,
+shipping_address_id int not null,
+ordered_quantity int not null,
+order_status varchar(20) not null,
+primary key(order_id,product_id),
+foreign key(order_id) references Orders(order_id),
+foreign key(product_id) references Product(id),
+foreign key(shipping_address_id) references Address(address_id)
+);
+
+show tables;
