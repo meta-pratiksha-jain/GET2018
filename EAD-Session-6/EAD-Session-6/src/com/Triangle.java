43edbff433cd182c2eb6f613476a1e52bd49fee4
meta-pratiksha-jain <41181700+meta-pratiksha-jain@users.noreply.github.com>
2018-09-02 19:02:27 +0530

    EAD-Session-6

---
+++ EAD-Session-6/EAD-Session-6/src/com/Triangle.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Triangle {
+	
+	public double getArea(double side1,double side2,double side3)
+	{
+		//calculates area of triangle using heron's formula
+		double p=(side1+side2+side3)/2;
+		double area=Math.sqrt(p*(p-side1)*(p-side2)*(p-side3));
+		return area;
+	}
+
+}
